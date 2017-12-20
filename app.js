f88004900e1f5de0fc374617ebf69be81d6f4858
Luiz Guilherme Ferrari <lgm.ferrari@hotmail.com>
2017-12-20 21:08:06 -0200

    First server using express

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 let express = require('express')
 let app = express()
+
 const port = 3000
+
+const template = 
+`<html>
+    <body>
+        <h1>All Products</h1>
+    </body>
+</html>`
+
+app
+    .get(`/products`, (request, response) => {
+        response.send(template)
+    })
 
 app
     .listen(port, () => {
