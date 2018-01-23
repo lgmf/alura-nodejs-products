eb26c9f6fec75b36294d00e7e49babce5882fb03
Luiz Guilherme Mendes Ferrari <luiz.guilherme@forlogic.net>
2018-01-22 22:39:59 -0200

    validação para caso houver erro na consulta ao BD

--- app/routes/products.js
+++ app/routes/products.js
@@ -3,12 +3,14 @@ module.exports = (app) => {
         .get(`/products`, (req, res) => {
 
             let dbConnection = app.shared.connectionFactory();
-            let db = app.shared.controllers.productsDB;
+            let db = app.shared.models.productsDB;
 
-            db.list(dbConnection, function (erros, resultados) {
-                res.render('products/products-list/list', { lista: resultados });
+            db.list(dbConnection, function (err, results) {
+                if (err) res.status(500).send(err);
+
+                res.render('products/products-list/list', { list: results });
             });
 
-            dbConnection.end(); 
+            dbConnection.end();
         });
 }
